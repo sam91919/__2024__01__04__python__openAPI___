147153b1662655f5865f43ab62cc1a1fba69727a
John Doe <johndoe@example.com>
2024-01-12 16:07:10 +0800

    113

---
+++ pico_w (19991024)/lession6_1~5.py
@@ -0,0 +1,11 @@
+import machine
+import time
+from machine import Pin
+
+led25 = Pin("",Pin.OUT)
+while True:
+    print(machine.freq())
+    led25.value(1)
+    time.sleep(1)
+    led25.value(0)
+    time.sleep(1)
