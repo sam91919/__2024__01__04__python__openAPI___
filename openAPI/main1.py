cc4f0c05d5655cc507fc7d41613422ade46123b1
John Doe <johndoe@example.com>
2024-01-22 11:03:47 +0800

    5688

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -13,7 +13,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/item/{item_id}")
+@app.get("/items/{item_id}")
 async def get_item(item_id):
     print(f"使用者輸入了:{item_id}")
     return{"item_id":item_id}
