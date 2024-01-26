7739cd97163d0c7c16995ec2c17e247cf2c01987
John Doe <johndoe@example.com>
2024-01-26 11:13:36 +0800

    4949

---
+++ UI/main.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+
+url = 'https://openapi-test-6vnu.onrender.com/pico_w/?count=5'
+
+r = requests.get(url=url)
+
+if r.status_code == 200:
+    print("下載成功")
+    data = r.json()
+
+print(data)
+pd.DataFrame(data)
